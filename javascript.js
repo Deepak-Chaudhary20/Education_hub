871aad6ebcd4eb77caff7f9d51e333c2adbf0b4c
Deepak Chaudhary <101586061+Deepak-Chaudhary20@users.noreply.github.com>
2022-10-03 19:51:09 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$("#btno").click(function(){
+    $("#mynavBar").css("background-color","rgb(102, 102, 255)");
+});
+$(function(){
+    window.onload(function(){
+        $("#alert").show();
+    });
+});
